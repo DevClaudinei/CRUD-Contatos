927752a33c47094fd5e63a5ad0b25a2f0e6b91fa
Claudinei Santos <claudineibitta@gmail.com>
2023-09-18 12:42:20 -0300

    feat: criacao de fixture para person

---
+++ backend/UnitTests/Fixture/PersonFixture.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using DomainModels.Models;
+using System.Collections.Generic;
+
+namespace UnitTests.Fixture
+{
+    public static class PersonFixture
+    {
+        public static Persons CreatePerson()
+        {
+            var faker = new Faker<Persons>()
+                .RuleFor(p => p.Id, f => f.Random.Long())
+                .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+                .RuleFor(p => p.LastName, f => f.Person.LastName)
+                .RuleFor(p => p.Age, f => f.Random.Int(18, 200))
+                .RuleFor(p => p.Profession, f => f.Random.String());
+
+            return faker.Generate();
+        }
+
+        public static IEnumerable<Persons> CreatePersons(int quantity)
+        {
+            var faker = new Faker<Persons>()
+                .RuleFor(p => p.Id, f => f.IndexFaker)
+                .RuleFor(p => p.FirstName, f => f.Person.FirstName)
+                .RuleFor(p => p.LastName, f => f.Person.LastName)
+                .RuleFor(p => p.Age, f => f.Random.Int(18, 200))
+                .RuleFor(p => p.Profession, f => f.Random.String())
+                .Generate(quantity);
+
+            return faker;
+        }
+    }
+}
