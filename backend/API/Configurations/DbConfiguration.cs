45691b47d5006a1194d2528ae83d69da11074498
Claudinei Santos <claudineibitta@gmail.com>
2023-09-18 12:33:12 -0300

    feat: criacao de configuracoes de banco

---
+++ backend/API/Configurations/DbConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using EntityFrameworkCore.UnitOfWork.Extensions;
+using Infraestructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Configurations
+{
+    public static class DbConfiguration
+    {
+        public static void AddDbConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                                b => b.MigrationsAssembly("Infraesctructure.Data"));
+            }, contextLifetime: ServiceLifetime.Transient);
+            services.AddUnitOfWork<DataContext>(ServiceLifetime.Transient);
+        }
+    }
+}
